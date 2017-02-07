4564ec6c582730c8f1b81c00e9c53128e9390443
Maxim Tschumak <maxim.tschumak@gmail.com>
2017-02-07 12:12:48 +0100

    swaping instagram api definition to uber (swagger's default one)

--- storage/load-test-data.sh
+++ storage/load-test-data.sh
@@ -4,7 +4,7 @@
 set -e
 set -o xtrace
 
-base_url=${1:-localhost:8080}
+base_url=${1:-localhost:8010}
 
 curl -H "Content-Type: application/json" -X PUT --data @src/test/resources/petstore-full.json ${base_url}/apps/petstore
-curl -H "Content-Type: application/json" -X PUT --data @src/test/resources/instagram.json ${base_url}/apps/instagram
+curl -H "Content-Type: application/json" -X PUT --data @src/test/resources/uber.json ${base_url}/apps/uber
