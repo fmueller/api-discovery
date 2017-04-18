5a85593b25088a954b9769fefd15329e21b62a91
Maxim Tschumak <maxim.tschumak@gmail.com>
2017-04-18 09:14:25 +0200

    #69 introducing lombok

--- crawler/src/main/java/org/zalando/apidiscovery/crawler/storage/ApiDefinition.java
+++ crawler/src/main/java/org/zalando/apidiscovery/crawler/storage/ApiDefinition.java
@@ -2,13 +2,19 @@ package org.zalando.apidiscovery.crawler.storage;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ApiDefinition {
 
-    public static ApiDefinition UNSUCCESSFUL = new ApiDefinition(
-            "UNSUCCESSFUL", null, null, null, null, null, null, null, null
-    );
+    public static ApiDefinition UNSUCCESSFUL = ApiDefinition.builder().status("UNSUCCESSFUL").build();
 
     @JsonProperty("status")
     private String status;
@@ -37,94 +43,4 @@ public class ApiDefinition {
     @JsonProperty("definition")
     private String definition;
 
-    public ApiDefinition() {
-    }
-
-    public ApiDefinition(String status, String type,
-                         String apiName, String appName,
-                         String version, String serviceUrl,
-                         String schemaUrl, String uiLink,
-                         String definition) {
-        this.status = status;
-        this.type = type;
-        this.apiName = apiName;
-        this.appName = appName;
-        this.version = version;
-        this.serviceUrl = serviceUrl;
-        this.schemaUrl = schemaUrl;
-        this.uiLink = uiLink;
-        this.definition = definition;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getApiName() {
-        return apiName;
-    }
-
-    public void setApiName(String apiName) {
-        this.apiName = apiName;
-    }
-
-    public String getAppName() {
-        return appName;
-    }
-
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getServiceUrl() {
-        return serviceUrl;
-    }
-
-    public void setServiceUrl(String serviceUrl) {
-        this.serviceUrl = serviceUrl;
-    }
-
-    public String getSchemaUrl() {
-        return schemaUrl;
-    }
-
-    public void setSchemaUrl(String schemaUrl) {
-        this.schemaUrl = schemaUrl;
-    }
-
-    public String getUiLink() {
-        return uiLink;
-    }
-
-    public void setUiLink(String uiLink) {
-        this.uiLink = uiLink;
-    }
-
-    public String getDefinition() {
-        return definition;
-    }
-
-    public void setDefinition(String definition) {
-        this.definition = definition;
-    }
 }
--- crawler/src/main/java/org/zalando/apidiscovery/crawler/storage/LegacyApiDefinition.java
+++ crawler/src/main/java/org/zalando/apidiscovery/crawler/storage/LegacyApiDefinition.java
@@ -2,13 +2,19 @@ package org.zalando.apidiscovery.crawler.storage;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LegacyApiDefinition {
 
-    public static LegacyApiDefinition UNSUCCESSFUL = new LegacyApiDefinition(
-            "UNSUCCESSFUL", null, null, null, null, null, null, null
-    );
+    public static LegacyApiDefinition UNSUCCESSFUL = LegacyApiDefinition.builder().status("UNSUCCESSFUL").build();
 
     @JsonProperty("status")
     private String status;
@@ -34,84 +40,4 @@ public class LegacyApiDefinition {
     @JsonProperty("definition")
     private String definition;
 
-    public LegacyApiDefinition() {
-    }
-
-    public LegacyApiDefinition(String status, String type,
-                               String name, String version,
-                               String serviceUrl, String schemaUrl,
-                               String uiLink, String definition) {
-        this.status = status;
-        this.type = type;
-        this.name = name;
-        this.version = version;
-        this.serviceUrl = serviceUrl;
-        this.schemaUrl = schemaUrl;
-        this.uiLink = uiLink;
-        this.definition = definition;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getServiceUrl() {
-        return serviceUrl;
-    }
-
-    public void setServiceUrl(String serviceUrl) {
-        this.serviceUrl = serviceUrl;
-    }
-
-    public String getSchemaUrl() {
-        return schemaUrl;
-    }
-
-    public void setSchemaUrl(String schemaUrl) {
-        this.schemaUrl = schemaUrl;
-    }
-
-    public String getUiLink() {
-        return uiLink;
-    }
-
-    public void setUiLink(String uiLink) {
-        this.uiLink = uiLink;
-    }
-
-    public String getDefinition() {
-        return definition;
-    }
-
-    public void setDefinition(String definition) {
-        this.definition = definition;
-    }
 }
