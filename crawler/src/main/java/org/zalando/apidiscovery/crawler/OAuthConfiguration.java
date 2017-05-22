e93e52913a9361f0aed7b0eb81c928034bd31cf5
Mario Schlegel <mario.schlegel@zalando.de>
2017-05-22 12:15:26 +0200

    #126 setup OAuthConfiguration and remove client tokens config from application context

--- crawler/src/main/java/org/zalando/apidiscovery/crawler/OAuthConfiguration.java
+++ crawler/src/main/java/org/zalando/apidiscovery/crawler/OAuthConfiguration.java
@@ -1,6 +1,5 @@
 package org.zalando.apidiscovery.crawler;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.stups.tokens.AccessTokens;
@@ -13,15 +12,12 @@ import java.net.URISyntaxException;
 @Configuration
 public class OAuthConfiguration {
 
-    @Value("${tokens.access-token-uri}")
-    private String accessTokenUri;
-
     @Bean
     public AccessTokens accessTokens() throws URISyntaxException {
-        // ManageToken and addScope are provided as dummy values due to weakness of the builder pattern.
-        // TokenIds and scopes are defined in the deployment sets.
-        // AccessTokens get fetched from the filesystem via org.zalando.stups.tokens.fs.FilesystemSecretRefresher
-        return Tokens.createAccessTokensWithUri(new URI(accessTokenUri))
+        // Uri, ManageToken and addScope are provided as dummy values due to insufficient design of the org.zalando.stups.tokens.AccessTokensBuilder.
+        // The values are defined in the deployment sets.
+        // AccessTokens are fetched from the filesystem via org.zalando.stups.tokens.fs.FilesystemSecretRefresher
+        return Tokens.createAccessTokensWithUri(new URI("https://dummy.zalando.com/"))
             .manageToken("dummy")
             .addScope("dummy")
             .done()
