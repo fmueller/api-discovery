389b545ebadc1218dab1196f3b89cdeff0a3480f
Mario Schlegel <mario.schlegel@zalando.de>
2017-06-27 15:39:18 +0200

    refactor CorsWithOAuthTest to component test #116

--- storage/src/test/java/org/zalando/apidiscovery/storage/resource/CorsWithOAuthTest.java
+++ storage/src/test/java/org/zalando/apidiscovery/storage/resource/CorsWithOAuthTest.java
@@ -6,24 +6,21 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.zalando.apidiscovery.storage.AbstractResourceIntegrationTest;
+import org.zalando.apidiscovery.storage.AbstractComponentTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("production")
-public class CorsWithOAuthTest extends AbstractResourceIntegrationTest {
+public class CorsWithOAuthTest extends AbstractComponentTest {
 
     @Test
-    public void shouldSupportCorsWhenOAuthIsEnabledOnAllResources() {
-        assertThat(optionsRequest("/apis")).isEqualTo(HttpStatus.OK);
-        assertThat(optionsRequest("/apis/dummy/versions")).isEqualTo(HttpStatus.OK);
-        assertThat(optionsRequest("/apis/dummy/versions/dummy/definitions/dummy")).isEqualTo(HttpStatus.OK);
-        assertThat(optionsRequest("/apis/dummy/deployments")).isEqualTo(HttpStatus.OK);
-        assertThat(optionsRequest("/applications")).isEqualTo(HttpStatus.OK);
-        assertThat(optionsRequest("/api-definitions")).isEqualTo(HttpStatus.OK);
-    }
-
-    private HttpStatus optionsRequest(String url) {
-        return restTemplate.exchange(url, HttpMethod.OPTIONS, RequestEntity.EMPTY, String.class).getStatusCode();
+    public void shouldSupportCorsWhenOAuthIsEnabledOnAllResources() throws Exception {
+        mvc.perform(options("/apis")).andExpect(status().isOk());
+        mvc.perform(options("/apis/dummy/versions")).andExpect(status().isOk());
+        mvc.perform(options("/apis/dummy/versions/dummy/definitions/dummy")).andExpect(status().isOk());
+        mvc.perform(options("/apis/dummy/deployments")).andExpect(status().isOk());
+        mvc.perform(options("/applications")).andExpect(status().isOk());
+        mvc.perform(options("/api-definitions")).andExpect(status().isOk());
     }
 }
