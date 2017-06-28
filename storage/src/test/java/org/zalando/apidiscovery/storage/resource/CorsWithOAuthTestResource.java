97dfe061c12cbe24d0acb940fc9edd09863da7db
Mario Schlegel <mario.schlegel@zalando.de>
2017-06-28 14:28:30 +0200

    refactor CorsWithOAuthTestResource as unit test #116

--- storage/src/test/java/org/zalando/apidiscovery/storage/resource/CorsWithOAuthTestResource.java
+++ storage/src/test/java/org/zalando/apidiscovery/storage/resource/CorsWithOAuthTestResource.java
@@ -2,14 +2,39 @@ package org.zalando.apidiscovery.storage.resource;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.zalando.apidiscovery.storage.AbstractResourceComponentTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.zalando.apidiscovery.storage.domain.service.ApiDefinitionProcessingService;
+import org.zalando.apidiscovery.storage.domain.service.ApiService;
+import org.zalando.apidiscovery.storage.domain.service.ApplicationService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
 @ActiveProfiles("production")
-public class CorsWithOAuthTestResource extends AbstractResourceComponentTest {
+@WebMvcTest(controllers = {
+    ApiDefinitionResourceController.class,
+    ApiResourceController.class,
+    ApplicationResourceController.class})
+public class CorsWithOAuthTestResource {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ApiDefinitionProcessingService apiDefinitionService;
+
+    @MockBean
+    private ApiService apiService;
+
+    @MockBean
+    private ApplicationService applicationService;
 
     @Test
     public void shouldSupportCorsWhenOAuthIsEnabledOnAllResources() throws Exception {
